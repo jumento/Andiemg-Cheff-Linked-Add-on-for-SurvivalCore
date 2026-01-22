c2d11d83dc07bea93ced3dd369e4870a6c647486
jumento <jumentolandia@gmail.com>
2026-01-22 08:39:52 -0600

    Bump version to 1.0.2 to reflect version check logic

--- src/main/java/com/jume/andiemgchefflinked/SurvivalCoreExtended.java
+++ src/main/java/com/jume/andiemgchefflinked/SurvivalCoreExtended.java
@@ -17,7 +17,7 @@ public class SurvivalCoreExtended extends JavaPlugin {
         super(init);
     }
 
-    public static final String VERSION = "1.0.1";
+    public static final String VERSION = "1.0.2";
 
     @Override
     protected void start() {
